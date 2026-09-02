dbd2f08498b395c50adfd7e6c2c280504ecb2329
HIMMY20 <chauhanmhimanshu2005@gmail.com>
2026-09-02 19:03:16 +0530

    frontend up

--- config/urls.py
+++ config/urls.py
@@ -1,27 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
-
-from products.sitemaps import ProductSitemap
-
-
-sitemaps = {
-    "products": ProductSitemap,
-}
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
     path("api/auth/", include("accounts.urls")),
     path("api/products/", include("products.urls")),
     path("api/orders/", include("orders.urls")),
-
-    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(
